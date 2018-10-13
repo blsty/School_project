4dcf5d3a30339334a1823c58e6799907f3ac09f1
Peter Blstak <blstak11peter@gmail.com>
2018-10-13 18:46:17 +0300

    created question path

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Subject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TeacherController extends AbstractController
@@ -11,12 +14,14 @@ class TeacherController extends AbstractController
 
     public function teacher()
     {
-        $id = $this->getUser()->getId();
-        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+       # $id = $this->getUser()->getId();
+        $user = $this->getUser();
+        $subjects = $this->getDoctrine()->getRepository(Subject::class)->findBy(["user"=>$user->getId()]);
+        $name = $user->getUsername();
 
-     $name = $user->getUsername();
+        return $this->render("teacher/welcome.html.twig", ["subjects" => $subjects, "name" => $name]);
 
-        return $this->render("teacher/welcome.html.twig", ["name" =>$name] );
+      #  return $this->render("teacher/welcome.html.twig", ["name" =>$name] );
     }
 
     public function create_subject(){
@@ -25,4 +30,44 @@ class TeacherController extends AbstractController
         return $this->render("teacher/create_subject.html.twig");
     }
 
+    public function formsubject(){
+        $request = Request::createFromGlobals()->request;
+        $subject = $request->get('subject');
+        $description = $request->get('description');
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $sbj = new Subject();
+        $sbj->setName($subject);
+        $sbj->setDescription($description);
+        $sbj->setUser($this->getUser());
+
+        $entityManager->persist($sbj);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('teacher');
+    }
+
+    public function formquestion(){
+        $request = Request::createFromGlobals()->request;
+        $question = $request->get('question');
+
+        return new Response($question);
+    }
+
+
+
+
+    public function create_exam(){
+
+        return $this->render("teacher/create_exam.html.twig");
+
+    }
+
+
+
+public function subject($id){
+
+    $subject = $this->getDoctrine()->getRepository(Subject::class)->find($id);
+    return $this->render("teacher/subject.html.twig",["subject"=>$subject]);
+}
 }
